cb22856941b1998471df313124d020073a0b764a
Afei Zhang <afei.zhang@boldseas.com>
2018-01-18 13:34:08 +0800

    加个包

--- Gradle-Two/src/main/java/Cglib.java
+++ Gradle-Two/src/main/java/Cglib.java
@@ -7,6 +7,9 @@ import java.lang.reflect.Method;
 
 public class Cglib {
     public static void main(String[] args){
+        InvocationHanderle();
+    }
+    public static void MethodInterceptor(){
         Bean asd = (Bean)Enhancer.create(Bean.class, new MethodInterceptor() {
             @Override
             public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
@@ -14,6 +17,18 @@ public class Cglib {
                     System.out.println(321);
                 }
                 methodProxy.invokeSuper(o,objects);
+                return null;
+            }
+        });
+        asd.asd();
+    }
+    public static void InvocationHanderle(){
+        Bean asd = (Bean)Enhancer.create(Bean.class, new InvocationHandler() {
+            @Override
+            public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+                if (method.getName().equals("asd")){
+                    System.out.println(321);
+                }
                 return null;
             }
         });
